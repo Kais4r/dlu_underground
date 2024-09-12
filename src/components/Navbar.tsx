aeea0b3f54cd16af7efd198807fce00f904af50f
Le Tran Anh Khoi <kais4r.dev@gmail.com>
2024-09-12 22:37:41 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <>
       {/* Navbar */}
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="flex flex-wrap justify-between items-center mx-auto p-4">
-          <a
+          {/* <a
             href="https://flowbite.com"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
@@ -16,7 +18,9 @@ export default function Navbar() {
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               DLU Underground
             </span>
-          </a>
+          </a> */}
+          <Link href="/">DLU Underground</Link>
+
           <div className="flex items-center space-x-6 rtl:space-x-reverse">
             <a
               href="tel:5541251234"
@@ -36,41 +40,26 @@ export default function Navbar() {
 
       {/* Navbar submenu */}
       <nav className="bg-gray-50 dark:bg-gray-700">
-        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+        <div className="px-4 py-3 mx-auto">
           <div className="flex items-center">
             <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/"
                   className="text-gray-900 dark:text-white hover:underline"
                   aria-current="page"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/pages/catalog"
                   className="text-gray-900 dark:text-white hover:underline"
+                  aria-current="page"
                 >
-                  Company
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="text-gray-900 dark:text-white hover:underline"
-                >
-                  Team
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="text-gray-900 dark:text-white hover:underline"
-                >
-                  Features
-                </a>
+                  Catalog
+                </Link>
               </li>
             </ul>
           </div>
