939c33d936191e20b9c729d9a55430fc47fca060
Le Trung Hoa Hieu <nothisu.dev@gmail.com>
2024-09-13 19:49:49 +0700

    feat: add card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,45 @@
+import { CardDemo } from "@/components/CardDemo";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <main>
-      <div>hello home page</div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="grid grid-flow-col grid-cols-3 gap-4">
+        <div>
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
     </main>
   );
 }
