eeabbe88866a63d53d7b5c107b3a6614c1fa3018
Le Trung Hoa Hieu <nothisu.dev@gmail.com>
2024-09-14 00:14:37 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { CardDemo } from "@/components/CardDemo";
 import {
   Card,
   CardContent,
@@ -8,14 +7,44 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+function Container({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        "flex items-center justify-center [&>div]:w-full",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="grid grid-flow-col grid-cols-3 gap-4">
-        <div>
-          <Card className="col-span-2">
+    <main className="min-h-screen">
+      <div className="grid h-80 grid-cols-5 gap-2  grid-rows-3">
+        <div className="flex items-center col-span-2 row-span-2">
+          <Card>
             <CardHeader>
-              <CardTitle>Card Title</CardTitle>
+              <CardTitle>Card Title 1</CardTitle>
+              <CardDescription>Card Description 1</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content 1</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+
+        <div className=" flex items-center col-span-3 row-span-1 bg-red-500">
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title 2</CardTitle>
               <CardDescription>Card Description</CardDescription>
             </CardHeader>
             <CardContent>
@@ -26,19 +55,10 @@ export default function Home() {
             </CardFooter>
           </Card>
         </div>
-
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+        <div className="col-span-1 row-span-2 bg-yellow-500" />
+        <div className="col-span-2 row-span-2 bg-green-500" />
+        <div className="col-span-1 row-span-1 bg-cyan-500" />
+        <div className="col-span-1 row-span-1 bg-purple-500" />
       </div>
     </main>
   );
